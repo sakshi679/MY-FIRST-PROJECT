2215d8f2b690f615bfdd82bf8f483fb28c5dd96b
sakshi pandey <pandeysai595@gmail.com>
2024-12-27 17:10:01 +0530

    write a new code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-let temperatures = [22 , 23 , 24 , 45 , 12 , 23 ,34];
-temperatures[3] = temperatures[3] + 2
-console.log(temperatures);
+let to_do_list = ["drink water" , "exercise" , "coding" , "reading" , "skincare"];
+to_do_list.unshift("praying");
+to_do_list.pop();
+console.log(to_do_list);
