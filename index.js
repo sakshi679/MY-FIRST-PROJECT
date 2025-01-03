f15a4ee77d0f7a2cfb812bf81953d77394024bd4
sakshi pandey <pandeysai595@gmail.com>
2025-01-03 21:41:23 +0530

    Solution for Pattern Printing III

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let N = 3 
-let M = 2
-let arr = [
-  [1 , 2],
-  [3 , 4],
-  [5 , 6]
-];
-
-for(let i=0; i<N; i++){
-  for(j=0; j<M; j++){
-    process.stdout.write(arr[i][j] + " "); 
+function printZigZag(matrix) {
+    let N = matrix.length;   // Number of rows
+    let M = matrix[0].length; // Number of columns
+  
+    // Traverse columns from right to left
+    for (let j = M - 1; j >= 0; j--) {
+      // If the column index is even, move down (top to bottom)
+      if ((M - j) % 2 != 0) {
+        for (let i = 0; i < N; i++) {
+          process.stdout.write(matrix[i][j] + " ");
+        }
+      }
+      // If the column index is odd, move up (bottom to top)
+      else {
+        for (let i = N - 1; i >= 0; i--) {
+          process.stdout.write(matrix[i][j] + " ");
+        }
+      }
+    }
   }
-  console.log();
-}
+  
+  // Example usage:
+  let matrix = [
+    [5, 4, 3],
+    [2, 1, 6],
+    [7, 8, 9]
+  ];
+  
+  printZigZag(matrix);  // Expected Output: 5 4 3 2 1 6 7 8 9
+  
 
